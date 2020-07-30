433ba605f2d84307a379a4a7443a5e43d8bdb682
kura-masahiro <dacanghongsheng@gmail.com>
2020-07-30 15:57:33 +0900

    ログイン機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+EMAIL = 'test@example.com'
+PASSWORD = 'password'
+
+user = User.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  puts 'ユーザーの初期データインポートに成功しました。'
+end
