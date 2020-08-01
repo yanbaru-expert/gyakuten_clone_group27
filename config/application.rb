9e6c0aa87a6254b25a0341b2870c8357c919efe3
elly <eri.patent@gmail.com>
2020-08-01 17:40:20 +0900

    add i18n.load_path to application.rb file

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module GyakutenCloneGroup
     config.generators.system_tests = nil
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
