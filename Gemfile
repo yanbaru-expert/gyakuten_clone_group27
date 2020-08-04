34f279fbcc9a770b3e1b8fef5a67a6d2ca848904
silverskg <chokichoki_gm@i.softbank.jp>
2020-08-04 21:34:48 +0900

    ユーザーの管理画面実装

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
+#管理者画面
+gem 'activeadmin'
+
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   #デバッグ用gemを追加
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
@@ -12,3 +5,4 @@ user = User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
