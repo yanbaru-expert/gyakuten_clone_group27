c1588e5ebbd8125574248616228bd938c948710a
silverskg <chokichoki_gm@i.softbank.jp>
2020-08-11 15:55:33 +0900

    seedファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-EMAIL = 'test@example.com'
+EMAIL = 'admin@example.com'
 PASSWORD = 'password'
 
-user = User.find_or_create_by!(email: EMAIL) do |user|
+# user = User.find_or_create_by!(email: EMAIL) do |user|
+#   user.password = PASSWORD
+#   puts 'ユーザーの初期データインポートに成功しました。'
+# end
+
+adminuser = User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
