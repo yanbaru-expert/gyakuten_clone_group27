b269cd8e19caf04dba19043217f5b16d5c06d05e
silverskg <chokichoki_gm@i.softbank.jp>
2020-08-11 17:13:33 +0900

    変数ASMIN_EMAIl追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-EMAIL = 'admin@example.com'
+EMAIL = 'test@example.com'
 PASSWORD = 'password'
+ADMIN_EMAIL = 'admin@example.com'
 
-# user = User.find_or_create_by!(email: EMAIL) do |user|
-#   user.password = PASSWORD
-#   puts 'ユーザーの初期データインポートに成功しました。'
-# end
-
-adminuser = User.find_or_create_by!(email: EMAIL) do |user|
+user = User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
-  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+end
+
+adminuser = User.find_or_create_by!(email: ADMIN_EMAIL) do |user|
+  user.password = PASSWORD
+  puts 'ユーザーの初期データインポートに成功しました。'
 end
 
