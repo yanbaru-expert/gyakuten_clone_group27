d2187521a53836974ff8423cad59f9d1d7553159
kura-masahiro <dacanghongsheng@gmail.com>
2020-08-12 17:53:03 +0900

    不要な記述の修正

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,12 +1,6 @@
 class TextsController < ApplicationController
   def index
     @texts = AwsText.order(id: :asc)
-     respond_to do |format|
-      format.html
-      format.csv do
-        send_data(@texts.generate_csv, filename: "aws_text_data.csv")
-      end
-    end
   end
 
 end
