c049e40e30297131a41bedce566cbed4d276bc81
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-01 21:55:45 +0900

    Rails動画教材ページの実装

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,18 @@
 class Movie < ApplicationRecord
   validates :title, :url, presence: true
+
+  def self.import(file)
+    CSV.foreach('db/csv_data/movie_data.csv', headers: true) do |row|
+      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+      movie = find_by(id: row["id"]) || new
+      # CSVからデータを取得し、設定する
+      movie.attributes = row.to_hash.slice(*updatable_attributes)
+      movie.save
+    end
+  end
+  
+  
+  def self.updatable_attributes
+    ['title', 'url']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: "movies#index"
+  root to: "texts#index"
   resources :aws_texts
+
+  resources :movies do
+    collection { post :import }
+  end
 end
