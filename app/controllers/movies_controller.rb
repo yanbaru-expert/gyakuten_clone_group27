e043e5141763aae6cb9500a3e7c4e0c473bcf930
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-07 17:13:12 +0900

    不要なコード削除

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,6 @@ class MoviesController < ApplicationController
   
   def index
       @movies = Movie.page(params[:page]).per(18).all
-      # @words = Word.page(params[:page]).per(PER)
   end
 
   def show
