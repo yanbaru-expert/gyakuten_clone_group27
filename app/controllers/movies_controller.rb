4b3f7684ca350729504ad274d4c283a32569ef9a
silverskg <chokichoki_gm@i.softbank.jp>
2020-11-17 21:13:00 +0900

    Movieテーブル共通化

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,14 @@
 class MoviesController < ApplicationController
-
-  
   def index
-      @movies = Movie.page(params[:page]).per(18).all
+    if params[:genre].nil? 
+      @movies = Movie.page(params[:page]).per(18).where(genre: ["Ruby on Rails", "Git","Basic","Ruby"])
+    #   @texts = Text.where(genre: ["Ruby on Rails", "Git","Basic","Ruby"])
+    # elsif params[:genre] == "Php"
+    #   @texts = Text.where(genre: "Php")
+    # else 
+    #   @texts = Text.where(genre: "AWS")
+    end
+      # @movies = Movie.page(params[:page]).per(18).all
   end
 
   def show
