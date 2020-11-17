93c5813731ef0f02a5211b870058b7940126d1cb
silverskg <chokichoki_gm@i.softbank.jp>
2020-11-17 20:09:47 +0900

    リンクにパラメーターを埋め込み共通化

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,12 @@
 class TextsController < ApplicationController
   def index
-    @texts = Text.all
+    if params[:genre].nil? 
+      @texts = Text.where(genre: ["Ruby on Rails", "Git","Basic","Ruby"])
+    elsif
+      @texts = Text.where(genre: "Php")
+    else
+      @texts = Text.where(genre: "AWS")
+    end
   end
 
   def show
