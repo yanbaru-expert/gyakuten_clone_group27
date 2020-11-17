d64ab8b674a70033560224cf4c123a267ae292dc
silverskg <chokichoki_gm@i.softbank.jp>
2020-11-18 07:01:05 +0900

    whereメソッド 各ジャンルをまとめる

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,23 +1,11 @@
 class MoviesController < ApplicationController
   def index
+    params[:genre]
     if params[:genre].nil? 
-      @movies = Movie.page(params[:page]).per(18).where(genre: ["Ruby on Rails", "Git","Basic","Ruby"])
-    elsif params[:genre] == "Live"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Live")
-    elsif params[:genre] == "Talk"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Talk")
-    elsif params[:genre] == "Movie"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Movie")
-    elsif params[:genre] == "Php"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Php")
-    elsif params[:genre] == "Writing"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Writing")
-    elsif params[:genre] == "Marketing"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Marketing")
-    elsif params[:genre] == "Money"
-      @movies = Movie.page(params[:page]).per(18).where(genre: "Money")
+      @movies = Movie.where(genre: ["Ruby on Rails", "Git","Basic","Ruby"]).page(params[:page]).per(18)
+    else
+      @movies = Movie.where(genre: params[:genre]).page(params[:page]).per(18)
     end
-      # @movies = Movie.page(params[:page]).per(18).all
   end
 
   def show
