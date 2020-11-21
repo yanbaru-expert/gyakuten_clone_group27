dfb0b42343eabb2276d3436b5e3ad9b45869333c
silverskg <chokichoki_gm@i.softbank.jp>
2020-11-21 20:26:11 +0900

    question　データベース作成

--- db/migrate/20201121110003_create_questions.rb
+++ db/migrate/20201121110003_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.text :detail
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_104436) do
+ActiveRecord::Schema.define(version: 2020_11_21_110003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,12 +48,27 @@ ActiveRecord::Schema.define(version: 2020_11_06_104436) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "lines", force: :cascade do |t|
+    t.string "genre"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "genre"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "texts", force: :cascade do |t|
