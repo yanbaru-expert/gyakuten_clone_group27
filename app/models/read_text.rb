380b30a2a8d5dd01d2d2599e648fa4e043355b53
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-05 21:29:02 +0900

    read_textモデル削除

--- app/models/read_text.rb
+++
@@ -1,5 +0,0 @@
-class ReadText < ApplicationRecord
-  belongs_to :user
-  belongs_to :text
-  validates_uniqueness_of :text_id, scope: :user_id
-end
--- db/migrate/20201129113758_create_read_texts.rb
+++
@@ -1,8 +0,0 @@
-class CreateReadTexts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :read_texts do |t|
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
