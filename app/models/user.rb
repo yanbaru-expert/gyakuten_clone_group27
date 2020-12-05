210a882af10e72c360015724095a2374afa10c90
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-05 19:49:21 +0900

    読んでいるかの判断

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :read_texts
+  def already_read?(text)
+    self.read_texts.exists?(text_id: text.id)
+  end
 end
