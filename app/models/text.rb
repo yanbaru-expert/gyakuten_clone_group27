866a7cb96f1e56ef31a8d48d33e62d59cd464404
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-09 17:29:17 +0900

    model/text 条件分岐の定義

--- app/models/text.rb
+++ app/models/text.rb
@@ -2,6 +2,9 @@ class Text < ApplicationRecord
   validates :title, :genre, :content, presence: true
   mount_uploader :image, ImageUploader
   belongs_to :user
-
   has_many :read_texts, dependent: :destroy
+
+  def read_by?(current_user)
+    read_texts.find_by(user_id: current_user.id).present?
+  end
 end
