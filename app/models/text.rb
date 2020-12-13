4242cac81ed6b9fd9e1eb3a4453205994f5b698b
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-13 20:10:56 +0900

    a

--- app/models/text.rb
+++ app/models/text.rb
@@ -4,7 +4,7 @@ class Text < ApplicationRecord
   has_many :read_texts, dependent: :destroy
   has_many :read_users, through: :read_texts, source: :user
 
-  def read_by?(current_user)
+  def rea_by?(current_user)
     read_texts.find_by(user_id: current_user.id).present?
   end
 end
