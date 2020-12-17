44a7fae9643591448fb58fd463d8e2f83863efee
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-17 10:58:14 +0900

    csvファイル　複数画像投稿-未定

--- lib/import.rb
+++ lib/import.rb
@@ -6,8 +6,12 @@ class Import
     # CSVファイルの（１行目をキーとして）各行のデータを配列に格納する
     # CSVファイルのカラム名と，テーブルのカラム名を一致させておく必要がある
     # row は CSV::Row クラスである。これはモデルの引数として渡せないのでハッシュに変換しておく。
+    
     list = []
-    CSV.foreach(path, headers: true) { |row| list << row.to_h }
+    
+    CSV.foreach(path, headers: true) { |row| list << row.to_h}
+    
     list
   end
+
 end
