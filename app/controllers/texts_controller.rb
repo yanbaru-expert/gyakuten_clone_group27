d77b197da37468d4c1063868062e362a8c91db01
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-22 20:18:48 +0900

    hidden_field_tag　使用

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,11 +1,11 @@
 class TextsController < ApplicationController
   def index
-  #  binding.pry
     if params[:genre].nil? 
       @eq = Text.where(genre: ["Ruby on Rails", "Git","Basic","Ruby"]).ransack(params[:q])
       @texts = @eq.result.order(:id)
     else
-      @texts = Text.where(genre: params[:genre])
+      @eq = Text.where(genre: params[:genre]).ransack(params[:q])
+      @texts = @eq.result.order(:id)
     end
   end
 
