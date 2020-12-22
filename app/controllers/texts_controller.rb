1a9fe29a54efc82550cc8b6517990bfdf1b6ea8f
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-22 21:59:44 +0900

    hidden_file_tag 条件分岐/@texts一つにまとめる

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,12 +1,12 @@
 class TextsController < ApplicationController
   def index
+    # binding.pry
     if params[:genre].nil? 
       @eq = Text.where(genre: ["Ruby on Rails", "Git","Basic","Ruby"]).ransack(params[:q])
-      @texts = @eq.result.order(:id)
     else
       @eq = Text.where(genre: params[:genre]).ransack(params[:q])
-      @texts = @eq.result.order(:id)
     end
+      @texts = @eq.result.order(:id)
   end
 
   def show
