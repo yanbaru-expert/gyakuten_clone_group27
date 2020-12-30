d1398430d7540392a0152d117eaef79ac746a4b9
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-30 17:26:48 +0900

    watched_moviesテーブルに必要カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_123100) do
+ActiveRecord::Schema.define(version: 2020_12_30_081225) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,16 +64,6 @@ ActiveRecord::Schema.define(version: 2020_12_05_123100) do
     t.string "genre"
   end
 
-  create_table "read_texts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "text_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["text_id"], name: "index_read_texts_on_text_id"
-    t.index ["user_id", "text_id"], name: "index_read_texts_on_user_id_and_text_id", unique: true
-    t.index ["user_id"], name: "index_read_texts_on_user_id"
-  end
-
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "detail"
@@ -111,6 +102,18 @@ ActiveRecord::Schema.define(version: 2020_12_05_123100) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "watched_movies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_watched_movies_on_movie_id"
+    t.index ["user_id", "movie_id"], name: "index_watched_movies_on_user_id_and_movie_id", unique: true
+    t.index ["user_id"], name: "index_watched_movies_on_user_id"
+  end
+
   add_foreign_key "read_texts", "texts"
   add_foreign_key "read_texts", "users"
+  add_foreign_key "watched_movies", "movies"
+  add_foreign_key "watched_movies", "users"
 end
