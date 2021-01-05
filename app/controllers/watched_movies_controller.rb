8f1b314e9163e711057e3068b5c2492f8ada392c
silverskg <chokichoki_gm@i.softbank.jp>
2021-01-05 21:27:57 +0900

    destroyアクション側も同じ内容記述

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,14 +1,11 @@
 class WatchedMoviesController < ApplicationController
   def create
     @movie = Movie.find(params[:movie_id])
-    # binding.pry
     current_user.watched_movies.create!(movie_id: @movie.id)
-    # current_user.watched_movies.create!(movie_id: params[:movie_id])
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    current_user.watched_movies.find_by(movie_id: params[:movie_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @movie = Movie.find(params[:movie_id])
+    current_user.watched_movies.find_by(movie_id: @movie.id).destroy!
   end
 end
