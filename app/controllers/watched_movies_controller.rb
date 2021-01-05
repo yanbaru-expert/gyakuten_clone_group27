f78570944759b44ca06c86be632ea3e2c6e0e6ed
silverskg <chokichoki_gm@i.softbank.jp>
2021-01-05 20:42:11 +0900

    インスタンス変数として作成

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,7 +1,10 @@
 class WatchedMoviesController < ApplicationController
   def create
-    current_user.watched_movies.create!(movie_id: params[:movie_id])
-    redirect_back(fallback_location: root_path)
+    @movie = Movie.find(params[:movie_id])
+    # binding.pry
+    current_user.watched_movies.create!(movie_id: @movie.id)
+    # current_user.watched_movies.create!(movie_id: params[:movie_id])
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
