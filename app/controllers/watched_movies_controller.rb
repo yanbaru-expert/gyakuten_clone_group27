dca45e5f4ee035e66a8a989d2881f5543f482da2
silverskg <chokichoki_gm@i.softbank.jp>
2021-01-08 18:26:24 +0900

    スペルミス等変更

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,12 +1,10 @@
 class WatchedMoviesController < ApplicationController
   def create
-    binding.pry
     @movie = Movie.find(params[:movie_id])
     current_user.watched_movies.create!(movie_id: @movie.id)
   end
 
   def destroy
-    binding.pry
     @movie = Movie.find(params[:movie_id])
     current_user.watched_movies.find_by(movie_id: @movie.id).destroy!
   end
