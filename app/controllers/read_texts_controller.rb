f1c8ba4fb33303c1321d50dbcb84e6678dc38d29
silverskg <chokichoki_gm@i.softbank.jp>
2021-01-08 15:40:59 +0900

    render送り先変更

--- app/controllers/read_texts_controller.rb
+++ app/controllers/read_texts_controller.rb
@@ -1,13 +1,15 @@
 class ReadTextsController < ApplicationController
 
   def create
-    # binding.pry
+    
     @text = Text.find(params[:text_id])
     current_user.read_texts.create!(text_id: @text.id)
   end
 
   def destroy
-    current_user.read_texts.find_by(text_id: params[:text_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @text = Text.find(params[:text_id])
+    current_user.read_texts.find_by(text_id: @text.id)
+    # current_user.read_texts.find_by(text_id: params[:text_id]).destroy!
+    # redirect_back(fallback_location: root_path)
   end
 end
