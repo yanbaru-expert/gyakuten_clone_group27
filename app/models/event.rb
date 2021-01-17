b93a151b35e9fd75091473b76b82db4dd40f4d63
silverskg <chokichoki_gm@i.softbank.jp>
2021-01-17 17:30:26 +0900

    バリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
 class Event < ApplicationRecord
   validates :title, :content, presence: true
+  validates :start_time, :end_time,  presence: true
+
+  validate :start_end_time
+  validate :start_check
+
+  def start_end_time
+    errors.add(:start_time, "は開始時刻より遅い時間を選択してください") if self.start_time > self.end_time
+  end
+
+  def start_check
+    errors.add(:start_time, "は現在の日時より遅い時間を選択してください") if self.start_time < Time.now
+  end
 end
