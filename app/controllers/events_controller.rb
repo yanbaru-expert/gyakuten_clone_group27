881d9d157e100b1b88818ef3773bc047499d23c4
silverskg <chokichoki_gm@i.softbank.jp>
2021-01-17 17:31:28 +0900

    end_time パラメータの許可

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,6 +45,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :content, :start_time)
+    params.require(:event).permit(:title, :content, :start_time, :end_time)
   end
 end
