13d270faffe0f4ad0e5a21a53e493c5734a68cec
silverskg <chokichoki_gm@i.softbank.jp>
2021-02-04 17:21:27 +0900

    要らない箇所削除

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,24 +13,12 @@ class EventsController < ApplicationController
   end
 
   def create
-  
     @event = Event.new(event_params)
     d = @event.start_day.values
     s = @event.start_time
     e = @event.end_time
     @event.start_time = Time.zone.local(d[0], d[1], d[2], s.hour, s.min)
     @event.end_time = Time.zone.local(d[0], d[1], d[2], e.hour, e.min)
-    # timezone = @event.start_day + @event.start_time
-    # timezone
-    # @event.start_time = @event.start_day
-    # @event.end_time = @event.start_day
-    # @event.start_time.to_i = @event.start_day
-    # @event.end_time = @event.start_day{1..3}
-    # @evevt
-    # binding.pry
-    # @event.start_time.save
-    # @event.end_time.save
-    # binding.pry
     if @event.save
       redirect_to events_path, notice: 'リストを作成しました'
     else
@@ -47,6 +35,7 @@ class EventsController < ApplicationController
 
   def edit
     @event = Event.find(params[:id])
+
   end
 
   def update
